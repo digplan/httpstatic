4d15eeced687a88c7e7b8a8779f1ceacbbd2bda9
digital planets <success@dpsw.info>
2015-01-08 23:55:12 -0600

    Update httpstatic.js

--- httpstatic.js
+++ httpstatic.js
@@ -1,15 +1,48 @@
-module.exports = function(use) {
+var fs = require('fs');
+var cache = {}, client;
+var livereload = process.env.livereload;
+var flag;
 
-  var fs = require('fs');
+if(livereload){
+ fs.watch('./static', function(){
+  cache = buildCache();
+  if(client)
+    setTimeout(function(){client.write('data:ok\n\n');}, 100);
+ });
+}
 
+function cli_reload(){
+  console.log('registering for reload');
+  var ev = new EventSource('/reload');
+  ev.onmessage = function(){
+    setTimeout(function(){history.go()}, 100); 
+    console.log('reloading');
+  };
+}
+var clijs = '\n\n<script>\n('+cli_reload.toString()+')()\n</script>';
+
+function buildCache(){
   var cache = {};
   fs.readdirSync('./static')
     .filter(function(f){ return !f.match(/^\.$/) })
     .forEach(function(f){
-      cache[f] = fs.readFileSync('./static/' + f).toString();
+      if(!f.match(/^\./) && !f.match(/\.TMP$/)) 
+        cache[f] = fs.readFileSync('./static/' + f).toString();
+      if(livereload) cache[f] += clijs;
     });
+  return cache;
+}
+
+module.exports = function(use) {
+  cache = buildCache();
 
   require('http').createServer(function(r, s) {
+
+    if(livereload && r.url == '/reload'){
+      s.setHeader('Content-Type', 'text/event-stream');
+      s.setHeader('Access-Control-Allow-Origin', '*');
+      return client = s;
+    }
 
     var d = '';
     r.on('data', function(chunk) {
@@ -18,6 +51,7 @@ module.exports = function(use) {
 
     r.on('end', function() {
 
+      s.setHeader('Content-Type', 'text/html');
       if(cache[r.headers.host])
       	return s.end(cache[r.headers.host]);
 
