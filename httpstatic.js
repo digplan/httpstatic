76933444650ce181caaf0e08f72c12960358390f
digital planets <success@dpsw.info>
2015-12-27 14:45:54 -0600

    Update httpstatic.js

--- httpstatic.js
+++ httpstatic.js
@@ -28,16 +28,50 @@ function httpstatic(func){
   }
 }
 
+function db(r, s){
+  switch(r.method){
+      case 'GET':
+        if(!db[table]) return s.end('table does not exist');
+        res = db[table]; break;
+      case 'POST':
+        if(!db[table]) return s.end('table does not exist');
+        var id = makeid();
+        var t = db[table];
+        if(Array.isArray(t)){
+          db[table].push(r.body);
+          res = db[table];
+        } else if(typeof t === 'string') {
+          res = db[table] = r.body;
+        } else if(typeof t === 'object') {
+          res = db[table][t] = r.body;
+        }
+        break;
+      case 'PATCH':
+        var id = r.body.id;
+        if(!db[table][id]) return s.end('no id provided');
+        delete r.body.id;
+        db[table][id] = r.body;
+        res = id; break;
+      case 'DELETE':
+        if(!db[table][r.body.id]) return s.end('no id provided');
+        delete db[table][r.body.id];
+        res = r.body.id; break;
+      case 'PUT':
+        if(db[table]) return s.end('table exists');
+        db[table] = r.body;
+        res = table;
+  }
+}
+
 module.exports = function(f, port){
+  var fu = httpstatic(f);
   // start server
   if(!fs.existsSync('certificate.crt'))
-    return http.createServer(f).listen(port||80);
+    return http.createServer(fu).listen(port||80);
 
   return https.createServer({
     ca: fs.readFileSync("./ca_bundle.crt"),
     key: fs.readFileSync("./private.key"),
     cert: fs.readFileSync("./certificate.crt")
-  }, f||df).listen(port||443);
-  
-  require('http').createServer(httpstatic(f)).listen(port || 80);
+  }, fu).listen(port||443);
 }
