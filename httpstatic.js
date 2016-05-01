e79a1d1d1ba41a8f2c45387e8bbe7f0c28adac48
digital planets <success@dpsw.info>
2016-05-01 11:17:23 -0500

    Update httpstatic.js

--- httpstatic.js
+++ httpstatic.js
@@ -1,8 +1,9 @@
 function httpstatic(func){
   var fs = require('fs'), 
-    cache = {}, 
+    cache = {}, colors = require('colors'),
     nocache = process.env.nocache, 
     dir = './static/';
+    if(!fs.existsSync(dir))  fs.mkdirSync(dir);
 
   require('node-dir').files(dir, function(err, files) {
     if (err) throw err;
@@ -21,57 +22,25 @@ function httpstatic(func){
         r.body = body;
         func && func(r, s);
         if(r.url === '/') r.url = '/index.html';
-        var path = dir + r.headers.host + r.url;
+        var path = dir + r.headers.host.match(/[^:]*/) + r.url;
+        
+        if(!fs.exists(path)) {
+          console.log(colors.red(path));
+          return s.end('');
+        }
+        console.log(colors.green(path));
         if(!nocache && cache[path]) s.end(cache[path]);
         s.end(fs.readFileSync(path).toString());
       });
   }
 }
 
-function db(r, s){
-  switch(r.method){
-      case 'GET':
-        if(!db[table]) return s.end('table does not exist');
-        res = db[table]; break;
-      case 'POST':
-        if(!db[table]) return s.end('table does not exist');
-        var id = makeid();
-        var t = db[table];
-        if(Array.isArray(t)){
-          db[table].push(r.body);
-          res = db[table];
-        } else if(typeof t === 'string') {
-          res = db[table] = r.body;
-        } else if(typeof t === 'object') {
-          res = db[table][t] = r.body;
-        }
-        break;
-      case 'PATCH':
-        var id = r.body.id;
-        if(!db[table][id]) return s.end('no id provided');
-        delete r.body.id;
-        db[table][id] = r.body;
-        res = id; break;
-      case 'DELETE':
-        if(!db[table][r.body.id]) return s.end('no id provided');
-        delete db[table][r.body.id];
-        res = r.body.id; break;
-      case 'PUT':
-        if(db[table]) return s.end('table exists');
-        db[table] = r.body;
-        res = table;
-  }
-}
-
-module.exports = function(f, port){
+module.exports = start = function(f, port){
   var fu = httpstatic(f);
-  // start server
-  if(!fs.existsSync('certificate.crt'))
-    return http.createServer(fu).listen(port||80);
+  port = process.env.port || port || 80;
+  console.log('Starting server on ' + port);
+  return require('http').createServer(fu).listen(port);
+} 
 
-  return https.createServer({
-    ca: fs.readFileSync("./ca_bundle.crt"),
-    key: fs.readFileSync("./private.key"),
-    cert: fs.readFileSync("./certificate.crt")
-  }, fu).listen(port||443);
-}
+var isScript = !module.parent;
+if(isScript) start();
