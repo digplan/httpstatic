97acded4389f1df7252399646e009bce9610c7b8
digital planets <success@dpsw.info>
2016-05-02 19:37:51 -0500

    Update httpstatic.js

--- httpstatic.js
+++ httpstatic.js
@@ -2,8 +2,11 @@ function httpstatic(func){
   var fs = require('fs'), 
     cache = {}, colors = require('colors'),
     nocache = process.env.nocache, 
-    dir = './static/';
-    if(!fs.existsSync(dir))  fs.mkdirSync(dir);
+    sep = require('path').sep,
+    dir = __dirname + sep + 'static' + sep;
+
+    if(!fs.existsSync(dir))
+      fs.mkdirSync(dir);
 
   require('node-dir').files(dir, function(err, files) {
     if (err) throw err;
@@ -20,24 +23,28 @@ function httpstatic(func){
       });
       r.on('end', function () {
         r.body = body;
-        func && func(r, s);
-        if(r.url === '/') r.url = '/index.html';
-        var path = dir + r.headers.host.match(/[^:]*/) + r.url;
-        
-        if(!fs.exists(path)) {
-          console.log(colors.red(path));
-          return s.end('');
+        if(func){
+          var ret = func(r, s);
+          if(ret) s.end();
         }
-        console.log(colors.green(path));
-        if(!nocache && cache[path]) s.end(cache[path]);
+        if(r.url === '/') r.url = sep + 'index.html';
+        var path = dir + r.headers.host.match(/[^:]*/) + r.url.replace('/', sep);
+
+        if(!fs.existsSync(path)){
+          console.log(colors.red(path.replace(dir,'')));
+          return s.end('')
+        }
+
+        console.log(colors.green(path.replace(dir,'')));
+        if(!nocache && cache[path]) return s.end(cache[path]);
         s.end(fs.readFileSync(path).toString());
       });
   }
 }
 
-module.exports = start = function(f, port){
+module.exports = start = function(f){
   var fu = httpstatic(f);
-  port = process.env.port || port || 80;
+  port = process.env.port || 80;
   console.log('Starting server on ' + port);
   return require('http').createServer(fu).listen(port);
 } 
